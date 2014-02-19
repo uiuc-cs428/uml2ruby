08ea13ecdaa4fda75f2dd7e38b6d4281706f3d68
deckerh <washulil@aim.com>
2014-02-18 19:04:59 -0800

    Generalization Ruby Unit Tests
    Added 5 Ruby unit tests that validate the generated Ruby code from
    Acceleo (from uml diagram containing generalizations) executes

---
+++ acceleo/src/rubyTests/ruby_test_example.rb
@@ -0,0 +1,34 @@
+require 'test/unit'
+require 'rubygems'
+require 'tuxml'
+require 'src/out/Food.rb'
+require 'src/out/Fruit.rb'
+require 'src/out/Meat.rb'
+require 'src/out/Apple.rb'
+require 'src/out/Banana.rb'
+require 'src/out/Beef.rb'
+
+
+class TestGeneralizationSuite < Test::Unit::TestCase
+  def test_apple_is_valid_class
+    pink_lady_apple = Apple.new
+    assert_not_nil(pink_lady_apple)
+  end
+
+  def test_apple_is_subclass_of_fruit
+    assert_equal(true, Apple < Fruit)
+  end
+
+  def test_fruit_not_subclass_of_apple
+    assert_equal(false, Fruit < Apple)
+  end
+
+  def test_parent_is_subclass_of_grandparent
+    assert_equal(true, Fruit < Food)
+  end
+  
+  def test_food_is_ancestor_of_banana_and_beef
+    assert_equal(true, Banana < Food)
+    assert_equal(true, Beef < Food)
+  end
+end
