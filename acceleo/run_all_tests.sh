39837bbff68768a536f416b1b5d9e79d74bfbad2
David Overgaard <enoch137@gmail.com>
2014-02-21 00:26:36 -0700

    Update run_all_tests.sh demo comment.

--- acceleo/run_all_tests.sh
+++ acceleo/run_all_tests.sh
@@ -1,6 +1,6 @@
 #Add a new line with a unique TUXML_OUTPUT_FILE name and Test::Unit file path
 #for each user story.
 
-#TUXML_OUTPUT_FILE=testUserStory.xml ruby testing/user_story/ruby_unit/TestClass.rb --runner xml
+#TUXML_OUTPUT_FILE=testUserStory.xml ruby acceleo/testing/user_story/ruby_unit/TestClass.rb --runner xml
 TUXML_OUTPUT_FILE=testClassGeneralization.xml ruby acceleo/testing/generalization/ruby_unit/generalization_unit_test.rb --runner xml
 echo 'Running Ruby unit tests'
