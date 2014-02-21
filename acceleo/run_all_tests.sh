4f175c02ea3ba6a3d66d13cf02fcd016ad1f5708
David Overgaard <enoch137@gmail.com>
2014-02-20 18:25:44 -0700

    Add run_all_tests.sh so that new tests can be configured to run with CI without having to modify the Jenkins config.

---
+++ acceleo/run_all_tests.sh
@@ -0,0 +1,4 @@
+#Add a new line with a unique TUXML_OUTPUT_FILE name and Test::Unit file path
+#for each user story.
+
+#TUXML_OUTPUT_FILE=testUserStory.xml ruby testing/user_story/autogen/TestClass.rb --runner xml
