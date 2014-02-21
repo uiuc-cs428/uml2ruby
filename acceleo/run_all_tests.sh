1d59bb52b11a93cac8952147f6688ba4412a8ac8
David Overgaard <enoch137@gmail.com>
2014-02-21 00:13:39 -0700

    Modify script path for Jenkins debug.

--- acceleo/run_all_tests.sh
+++ acceleo/run_all_tests.sh
@@ -2,5 +2,5 @@
 #for each user story.
 
 #TUXML_OUTPUT_FILE=testUserStory.xml ruby testing/user_story/ruby_unit/TestClass.rb --runner xml
-TUXML_OUTPUT_FILE=testClassGeneralization.xml ruby testing/generalization/ruby_unit/generalization_unit_test.rb --runner xml
+TUXML_OUTPUT_FILE=testClassGeneralization.xml ruby acceleo/testing/generalization/ruby_unit/generalization_unit_test.rb --runner xml
 echo 'Running Ruby unit tests'
