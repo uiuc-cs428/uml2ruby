b5df80df58589e1db4b80614ba5f32905233f69f
Kevin Boice <boicekev@gmail.com>
2014-03-02 17:05:22 -0500

    Updated the expected and test files for the comment functionality

--- acceleo/testing/comments/ruby_unit/comments_unit_test.rb
+++ acceleo/testing/comments/ruby_unit/comments_unit_test.rb
@@ -10,17 +10,9 @@ class CommentsClassSuite < Test::Unit::TestCase
     autogen = "acceleo/testing/comments/autogen/Class1.rb"
     expected = "acceleo/testing/comments/ruby_unit/Class1Expected.rb"
     
-    success = true
-    
     generatedCode = File.open(autogen).readlines
     expectedCode = File.open(expected).readlines
-    if generatedCode == expectedCode
-      puts "  [+] Class1.rb == Class1Expected.rb"
-    else
-      puts "  [-] Class1.rb != Class1Expected.rb"
-      success = false
-    end
-    success
+    assert_equal(generatedCode, expectedCode)
   end
 
 end
