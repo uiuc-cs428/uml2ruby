869710c934eda8f1182d41c387eafe8c7d238729
Jon Gill <jagill.vt@gmail.com>
2014-03-09 11:38:07 -0400

    add return value support for methods

--- acceleo/testing/class_methods/ruby_unit/class_methods_unit_test.rb
+++ acceleo/testing/class_methods/ruby_unit/class_methods_unit_test.rb
@@ -12,7 +12,7 @@ class TestMethodsSuite < Test::Unit::TestCase
 
   def test_class1_public_method_accessible
     c1 = Class1.new
-    assert(defined? c1.method1)
+    assert(defined? c1.pub_method1)
   end
 
   def test_class1_public_method_argument_list
@@ -21,14 +21,20 @@ class TestMethodsSuite < Test::Unit::TestCase
     arg2 = nil
     arg3 = nil
     assert_nothing_raised ArgumentError do
-      c1.method1(arg1, arg2, arg3)
+      c1.pub_method1(arg1, arg2, arg3)
     end
   end
 
-  def test_class2_private_method_unaccessible
+  def test_class1_return_value
+    c1 = Class1.new
+    assert(c1.pub_method1(nil, nil, nil) == nil)
+    assert(c1.pub_method2() == "")
+  end
+
+  def test_class1_private_method_unaccessible
     c1 = Class1.new
     assert_raise NoMethodError do
-      c1.method2()
+      c1.priv_method1()
     end
   end
 end
