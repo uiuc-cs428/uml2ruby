44c8b7efb92276ba57df825f34599af67ba37370
Jon Gill <jagill.vt@gmail.com>
2014-03-18 23:59:51 -0400

    fix comment test

--- acceleo/testing/comments/ruby_unit/comments_unit_test.rb
+++ acceleo/testing/comments/ruby_unit/comments_unit_test.rb
@@ -13,7 +13,8 @@ class CommentsClassSuite < Test::Unit::TestCase
       "# Attached Comment\n",
       "class Class1\n"
     ]
-    assert_equal(generated_code.include? expected_code_snippet)
+    puts generated_code.inspect
+    assert_equal(expected_code_snippet, generated_code[1..2])
   end
 
 end
