ef2026281da8c704d90f2bf1ab7ba2dceac6cc4c
Jon Gill <jagill.vt@gmail.com>
2014-03-18 22:03:24 -0400

    update unit test

--- acceleo/testing/big_class/ruby_unit/big_class_unit_test.rb
+++ acceleo/testing/big_class/ruby_unit/big_class_unit_test.rb
@@ -13,51 +13,134 @@ require_relative '../autogen/UMLDiagram.rb'
 class ClassDiagramIntegrationSuite < Test::Unit::TestCase
 
   def test_argument_class
+    argument = nil
     assert_nothing_raised NameError do
       argument = Argument.new
     end
+    assert_nothing_raised NoMethodError do
+      argument.name = "arg1"
+      argument.direction = "in"
+    end
+    assert_equal(argument.name, "arg1")
+    assert_equal(argument.direction, "in")
+    assert_equal(argument.default_value, nil)
   end
 
   def test_attribute_class
+    attribute = nil
     assert_nothing_raised NameError do
       attribute = Attribute.new
     end
+    assert_nothing_raised NoMethodError do
+      attribute.name = "name"
+      attribute.visibility = "public"
+      attribute.type = "String"
+    end
+    assert_equal(attribute.name, "name")
+    assert_equal(attribute.visibility, "public")
+    assert_equal(attribute.type, "String")
+    assert_equal(attribute.default_value, nil)
   end
 
+=begin
   def test_classdiagram_class
+    class_diagram = nil
     assert_nothing_raised NameError do
       class_diagram = ClassDiagram.new
     end
+    assert(ClassDiagram < UMLDiagram)
+
+    assert_nothing_raised NoMethodError do
+      class_diagram.classes = [ClassObject.new]
+    end
   end
+=end
 
   def test_classobject_class
+    food = nil
+    fruit = nil
+    apple = nil
     assert_nothing_raised NameError do
-      class_object = ClassObject.new
+      food = ClassObject.new
+      fruit = ClassObject.new
+      apple = ClassObject.new
     end
+    assert_nothing_raised NoMethodError do
+      food.name = "Food"
+      fruit.name = "Fruit"
+      apple.name = "Apple"
+
+      apple.parent = fruit
+      fruit.parent = food
+    end
+    assert_equal(apple.parent, fruit)
+    assert_equal(fruit.parent, food)
+
+    assert_equal(apple.name, "Apple")
+    assert_equal(apple.methods, nil)
+    assert_equal(apple.attributes, nil)
   end
 
   def test_generator_class
+    generator = nil
     assert_nothing_raised NameError do
       generator = Generator.new
     end
+
+    assert(defined? generator.generate_ruby)
+
+    code = nil
+    assert_nothing_raised ArgumentError do
+      code = generator.generate_ruby("path/to/uml", "./output")
+    end
+    assert_equal(code, "")
   end
 
   def test_method_class
+    method = nil
     assert_nothing_raised NameError do
       method = Method.new
     end
+    assert_nothing_raised NoMethodError do
+      method.name = "foo"
+      method.visibility = "public"
+      method.arguments = [Argument.new]
+    end
+    assert_equal(method.name, "foo")
+    assert_equal(method.visibility, "public")
+    assert_equal(method.arguments.size, 1)
   end
 
+=begin
   def test_sequencediagram_class
     assert_nothing_raised NameError do
       sequence_diagram = SequenceDiagram.new
     end
+    assert(SequenceDiagram < UMLDiagram)
+
+    assert_nothing_raised NoMethodError do
+      sequence_diagram.sequences = [SequenceObject.new]
+      sequence_diagram.classes = [ClassObject.new]
+    end
   end
+=end
 
   def test_sequenceobject_class
+    sequence_object = nil
     assert_nothing_raised NameError do
       sequence_object = SequenceObject.new
     end
+
+    foo = nil
+    bar = nil
+    assert_nothing_raised NoMethodError do
+      foo = Method.new
+      bar = Method.new
+      sequence_object.method_invocations = [foo, bar]
+    end
+
+    assert_equal(sequence.object.first, foo)
+    assert_equal(sequence.object.last, bar)
   end
 
   def test_umldiagram_class
