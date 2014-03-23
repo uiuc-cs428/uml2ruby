d850853bf6508445d70ae8b436bca04d1d4874ad
deckerh <washulil@aim.com>
2014-03-23 09:42:45 -0700

    ValueSpecificationAction
    Added .mtl code to support recognizing a ValueSpecificationAction which
    has an Output Pin result with a boolean value.

--- acceleo/testing/activity/autogen/SomeFunActivity.rb
+++ acceleo/testing/activity/autogen/SomeFunActivity.rb
@@ -1,22 +1,21 @@
-  #The activity "SomeFunActivity" has 7 nodes and 6 control flows
+  #The activity "SomeFunActivity" has 9 nodes and 7 control flows
   #The initial node is called "The Good Node"
   	#The Good Node
   	  #ControlFlow
-  	  	#DecisionNode
-  	  	  #ControlFlow1
-  	  	  	#OpaqueAction
-  	  	  	  #ControlFlow2
-  	  	  	  	#OpaqueAction1
-  	  	  	  	  #ControlFlow3
-  	  	  	  	  	#ActivityFinalNode
-  	  	  	  	  
-  	  	  	  
-  	  	  
-  	  	  #ControlFlow4
-  	  	  	#OpaqueAction2
-  	  	  	  #ControlFlow5
-  	  	  	  	#FlowFinalNode
-  	  	  	  
-  	  	  
-  	  
+        	#DecisionNode
+        	  #ControlFlow4
+              	#OpaqueAction2
+              	  #ControlFlow5
+                    #ValueSpecificationAction1 -> Output Pin: Result, Value: IsLightOn=false
+              
+        	  #ControlFlow1
+              	#OpaqueAction
+              	  #ControlFlow2
+                    	#OpaqueAction1
+                    	  #ControlFlow3
+                          	#ActivityFinalNode
+                          
+                    
+              
+        
   
