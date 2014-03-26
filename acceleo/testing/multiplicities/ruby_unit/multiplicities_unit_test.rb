692292bcdc95a3b848b1ee6569e703b8905a56cf
David Overgaard <enoch137@gmail.com>
2014-03-25 23:35:52 -0600

    Include local properties in the class diagram.

--- acceleo/testing/multiplicities/ruby_unit/multiplicities_unit_test.rb
+++ acceleo/testing/multiplicities/ruby_unit/multiplicities_unit_test.rb
@@ -8,14 +8,21 @@ class TestMultiplicitiesSuite < Test::Unit::TestCase
   def test_burger_attributes
     burger = Burger.new
     assert_not_nil(burger)
-
+    
     assert(burger.patties.kind_of? Array)
     assert(burger.patties.first.kind_of? Patty)
     assert(burger.bun == nil)
     assert(burger.condiments.kind_of? Array)
     assert(burger.condiments.size == 0)
     assert(burger.price.kind_of? Price)
+    
+    assert(burger._patties.kind_of? Array)
+    assert(burger._patties.first.kind_of? Patty)
+    assert(burger._bun == nil)
+    assert(burger._condiments.kind_of? Array)
+    assert(burger._condiments.size == 0)
+    assert(burger._condiments.first.kind_of != Condiment)
+    assert(burger._price.kind_of? Price)
   end
 
 end
-
