fbfdfa91e3ce80768eeb686dd67aa8c8384a61d0
Jon Gill <jagill.vt@gmail.com>
2014-04-07 19:55:59 -0400

    add model

---
+++ acceleo/testing/decision_merge/ruby_unit/decision_merge_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require 'rubygems'
+require 'tuxml'
+require_relative '../autogen/DecisionMerge.rb'
+
+class TestActivityDecisionMergeSuite < Test::Unit::TestCase
+  def empty_test()
+    assert(true)
+  end
+end
