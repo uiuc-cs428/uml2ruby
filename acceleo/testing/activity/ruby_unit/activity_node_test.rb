8095297676ed7924d3a318e30505a1a79337ef38
deckerh <washulil@aim.com>
2014-04-12 12:27:06 -0700

    Redid unit tests

--- acceleo/testing/activity/ruby_unit/activity_node_test.rb
+++ acceleo/testing/activity/ruby_unit/activity_node_test.rb
@@ -33,6 +33,24 @@ class TestActivityNodeSuite < Test::Unit::TestCase
   end
   
   def test_call_behavior_action_method_defined
-      stringInFile('acceleo/testing/activity/autogen/SomeFunActivity.rb', 'returnOrder =BehaviorActivity(order)')
+    stringInFile('acceleo/testing/activity/autogen/SomeFunActivity.rb', 'returnOrder =BehaviorActivity(order)')
+  end
+  
+  def test_behavior_activity
+    stringInFile('acceleo/testing/activity/autogen/BehaviorActivity.rb', 'def BehaviorActivity(order)')
+  end
+  
+  def test_behavior_activity_parameter_node_initialization
+    stringInFile('acceleo/testing/activity/autogen/BehaviorActivity.rb', 'anOrder = order')
+    stringInFile('acceleo/testing/activity/autogen/BehaviorActivity.rb', 'anReturnOrder = nil')
+  end
+  
+  def test_behavior_activity_opaque_actions
+    stringInFile('acceleo/testing/activity/autogen/BehaviorActivity.rb', 'RushedDelivery()')
+    stringInFile('acceleo/testing/activity/autogen/BehaviorActivity.rb', 'RegularDelivery()')
+  end
+  
+  def test_behavior_activity_opaque_actions
+    stringInFile('acceleo/testing/activity/autogen/BehaviorActivity.rb', 'return anReturnOrder')
   end
 end
---
+++ acceleo/testing/activity_parameter_node/ruby_unit/activity_parameter_node_test.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require 'rubygems'
+require 'tuxml'
+
+class TestActivityNodeSuite < Test::Unit::TestCase
+  def stringInFile(fileName, string)
+    generatedCode = File.open(fileName).read
+    assert(generatedCode.include?(string))
+  end
+    
+  def test_activity_behavior_defined
+    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'def Activity1(input1,')
+  end
+  
+  def test_activity_parameter_node_initialization
+    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput1 = input1')
+    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput2 = input2')
+    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput3 = nil')
+  end
+  
+  def test_activity_return_statement
+    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'return anInput3')
+  end
+end
