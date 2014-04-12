dd9306ed87480ad0df3e0936473a5f27917129e5
David Overgaard <enoch137@gmail.com>
2014-04-12 08:06:19 -0600

    Add more test cases and make test suite unique.

--- acceleo/testing/activity_call_operation/ruby_unit/call_operation_node_test.rb
+++ acceleo/testing/activity_call_operation/ruby_unit/call_operation_node_test.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require 'rubygems'
 require 'tuxml'
 
-class TestActivityNodeSuite < Test::Unit::TestCase
+class TestActivityCallOperationNodeSuite < Test::Unit::TestCase
   def assertRegexInFile(fileName, string)
     assert(regexFoundInFile(fileName, string), "String not found: " + string)      
   end
@@ -19,7 +19,15 @@ class TestActivityNodeSuite < Test::Unit::TestCase
     end
   end
   
-  def test_initial_node
+  def test_result_assigned_to_class
     assertRegexInFile('acceleo/testing/activity_call_operation/autogen/SomeFunActivity.rb', 'SaveThis\=Class1.operation1_from_class\(Parameter1\)')
+  end
+  
+  def test_class_operation_without_result
+    assertRegexInFile('acceleo/testing/activity_call_operation/autogen/SomeFunActivity.rb', '^\s*Class1.operation2_from_class\(Parameter1\)')
+  end
+  
+  def test_class_operation_with_no_parameter
+    assertRegexInFile('acceleo/testing/activity_call_operation/autogen/SomeFunActivity.rb', '^\s*Class1.operation3_from_class\(\)')
   end
 end
