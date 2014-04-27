9c1ae835dc4351c109785478fd7f628002341186
David Overgaard <enoch137@gmail.com>
2014-04-27 09:14:53 -0600

    Add comments to Java query code and refactor method sortParamSet to sortMethodParam.

--- acceleo/src/acceleo/main/AcceleoHelperQueries.java
+++ acceleo/src/acceleo/main/AcceleoHelperQueries.java
@@ -23,7 +23,20 @@ import org.eclipse.uml2.uml.internal.impl.TypedElementImpl;
  * https://github.com/uiuc-cs428/uml2ruby/wiki/Query-documentation
  */
 public class AcceleoHelperQueries {
-	public Set<String> sortParamSet(Set<ParameterImpl> inputStrings) {
+
+	/**
+	 * Return an alphabetical set of parameter names so that the class and
+	 * activity diagrams call methods in the same order.
+	 * 
+	 * The first character of the parameter is forced to lower case to prevent
+	 * declaring a constant in Ruby.
+	 * 
+	 * @param inputStrings
+	 *            The collection of parameters associated with the method
+	 *            definition or call.
+	 * @return An alphabetical set of parameter names.
+	 */
+	public Set<String> sortMethodParam(Set<ParameterImpl> inputStrings) {
 		Set<String> sortedParams = new TreeSet<String>();
 
 		for (ParameterImpl param : inputStrings) {
@@ -32,25 +45,55 @@ public class AcceleoHelperQueries {
 				sortedParams.add(firstCharToLower(param.getName().toString()));
 			}
 		}
-		//
-		// System.out.println("Sorted:");
-		// for (String string : sortedParams) {
-		// System.out.println("argument: " + string);
-		// }
-		//
-		// System.out.println("hello world");
 
 		return sortedParams;
 	}
 
+	/**
+	 * Return an alphabetical set of parameter names so that the multiple
+	 * activity diagrams will call each other as methods in the same order.
+	 * 
+	 * The first character of the parameter is forced to lower case to prevent
+	 * declaring a constant in Ruby.
+	 * 
+	 * Note: This class was extracted from .mtl and keeping the same logic.
+	 * Potential to refactor this method with sortParamSet and other methods
+	 * from the .mtl file.
+	 * 
+	 * @param inputStrings
+	 *            The collection of parameters associated with the activity
+	 *            definition or call.
+	 * @return An alphabetical set of parameter names.
+	 */
+	public Set<String> sortActivityParam(Set<ParameterImpl> test) {
+		Set<String> sortedParams = new TreeSet<String>();
+
+		for (ParameterImpl param : test) {
+			if (param.getDirection().toString().equals("in")) {
+				sortedParams.add(firstCharToLower(param.getName().toString()));
+			}
+		}
+
+		return sortedParams;
+	}
+
+	/**
+	 * Format method parameters for use in documentation about their intended
+	 * type.
+	 * 
+	 * @param inputStrings
+	 *            The collection of parameters associated with the method
+	 *            definition or call.
+	 * @return A formated set of parameter names and types.
+	 */
 	public Set<String> sortParamComment(Set<ParameterImpl> inputStrings) {
 		Set<String> sortedParams = new TreeSet<String>();
 
 		for (ParameterImpl param : inputStrings) {
 			if (!(param.getDirection().toString().equals("out") || param
-					.getDirection().toString().equals("return"))) {			
-				sortedParams.add(firstCharToLower("#   - +"
-						+ param.getName().toString())
+					.getDirection().toString().equals("return"))) {
+				sortedParams.add("#   - +"
+						+ firstCharToLower(param.getName().toString())
 						+ "+ -> " + getType(param.getType()));
 			}
 		}
@@ -58,30 +101,27 @@ public class AcceleoHelperQueries {
 		return sortedParams;
 	}
 
-	public Set<String> sortActivityParam(Set<ParameterImpl> test) {
-		// System.out.println("hello world");
-		Set<String> sortedParams = new TreeSet<String>();
-
-		for (ParameterImpl param : test) {
-			// System.out.println("argument: " + param);
-			// System.out.println("direction: " + param.getDirection());
-			if (param.getDirection().toString().equals("in")) {
-				// System.out.println("name: " +
-				// firstCharToLower(param.getName().toString()));
-				sortedParams.add(firstCharToLower(param.getName().toString()));
-			}
-		}
-		// for (OCL ocl : inputParams) {
-		// System.out.println("arguemnt: " + ocl);
-		// }
-
-		return sortedParams;
-	}
-
+	/**
+	 * Change first character of a string to lower case
+	 * 
+	 * @param param
+	 *            String to manipulate
+	 * @return The param string with the first character forced to lower case
+	 */
 	private String firstCharToLower(String param) {
 		return param.substring(0, 1).toLowerCase() + param.substring(1);
 	}
 
+	/**
+	 * Get the Ruby type associated from the given type class.
+	 * 
+	 * Primitive types containing the following names will be set in this way:
+	 * String -> String Integer -> Fixnum Real -> Float
+	 * 
+	 * @param typeData
+	 *            Data class containing parameter type information
+	 * @return The name of the type associated with this type data class
+	 */
 	private String getType(Type typeData) {
 		String typeName;
 		if (typeData == null) {
