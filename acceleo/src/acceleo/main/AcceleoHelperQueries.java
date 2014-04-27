e364bb02a61c7560a715b5ac3437d72916b65260
David Overgaard <enoch137@gmail.com>
2014-04-27 01:14:48 -0600

    Ensure that calls to other activities also have sorted parameters.  Clean up how parameter comments are being printed.

--- acceleo/src/acceleo/main/AcceleoHelperQueries.java
+++ acceleo/src/acceleo/main/AcceleoHelperQueries.java
@@ -3,7 +3,11 @@ package acceleo.main;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.eclipse.ocl.OCL;
+import org.eclipse.ocl.ecore.OCLExpression;
+import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.internal.impl.ParameterImpl;
+import org.eclipse.uml2.uml.internal.impl.TypedElementImpl;
 
 /**
  * This is a helper class used to make Java service calls from Acceleo .mtl
@@ -22,19 +26,86 @@ public class AcceleoHelperQueries {
 	public Set<String> sortParamSet(Set<ParameterImpl> inputStrings) {
 		Set<String> sortedParams = new TreeSet<String>();
 
-		for (ParameterImpl object : inputStrings) {
-//			System.out.println("argument: " + object.getName());
-//			System.out.println("argument: " + object);
-			sortedParams.add(object.getName());
+		for (ParameterImpl param : inputStrings) {
+			if (!(param.getDirection().toString().equals("out") || param
+					.getDirection().toString().equals("return"))) {
+				sortedParams.add(firstCharToLower(param.getName().toString()));
+			}
 		}
-//
-//		System.out.println("Sorted:");
-//		for (String string : sortedParams) {
-//			System.out.println("argument: " + string);
-//		}
-//
-//		System.out.println("hello world");
+		//
+		// System.out.println("Sorted:");
+		// for (String string : sortedParams) {
+		// System.out.println("argument: " + string);
+		// }
+		//
+		// System.out.println("hello world");
 
 		return sortedParams;
+	}
+
+	public Set<String> sortParamComment(Set<ParameterImpl> inputStrings) {
+		Set<String> sortedParams = new TreeSet<String>();
+
+		for (ParameterImpl param : inputStrings) {
+			if (!(param.getDirection().toString().equals("out") || param
+					.getDirection().toString().equals("return"))) {			
+				sortedParams.add(firstCharToLower("#   - +"
+						+ param.getName().toString())
+						+ "+ -> " + getType(param.getType()));
+			}
+		}
+
+		return sortedParams;
+	}
+
+	public Set<String> sortActivityParam(Set<ParameterImpl> test) {
+		// System.out.println("hello world");
+		Set<String> sortedParams = new TreeSet<String>();
+
+		for (ParameterImpl param : test) {
+			// System.out.println("argument: " + param);
+			// System.out.println("direction: " + param.getDirection());
+			if (param.getDirection().toString().equals("in")) {
+				// System.out.println("name: " +
+				// firstCharToLower(param.getName().toString()));
+				sortedParams.add(firstCharToLower(param.getName().toString()));
+			}
+		}
+		// for (OCL ocl : inputParams) {
+		// System.out.println("arguemnt: " + ocl);
+		// }
+
+		return sortedParams;
+	}
+
+	private String firstCharToLower(String param) {
+		return param.substring(0, 1).toLowerCase() + param.substring(1);
+	}
+
+	private String getType(Type typeData) {
+		String typeName;
+		if (typeData == null) {
+			typeName = "Undefined";
+		} else if (typeData.isSetName()) {
+			typeName = typeData.getName().toString();
+		} else {
+			String typeDataString = typeData.toString();
+
+			String primitiveType = typeDataString.substring(
+					typeDataString.lastIndexOf('#') + 1,
+					typeDataString.length() - 1);
+
+			if (primitiveType.contains("String")) {
+				typeName = "String";
+			} else if (primitiveType.contains("Integer")) {
+				typeName = "Fixnum";
+			} else if (primitiveType.contains("Real")) {
+				typeName = "Float";
+			} else {
+				typeName = primitiveType;
+			}
+		}
+
+		return typeName;
 	}
 }
