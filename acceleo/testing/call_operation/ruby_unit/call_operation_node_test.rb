4bd04c1039db4177ceb92dae0448b902b9645b06
David Overgaard <enoch137@gmail.com>
2014-04-26 19:47:46 -0600

    Add duplicate require check for activity diagrams.  Correct duplicate require check for class diagram.

--- acceleo/testing/call_operation/ruby_unit/call_operation_node_test.rb
+++ acceleo/testing/call_operation/ruby_unit/call_operation_node_test.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 require 'rubygems'
 require 'tuxml'
+require 'set'
 
 class TestActivityCallOperationNodeSuite < Test::Unit::TestCase
   def assertRegexInFile(fileName, string)
@@ -20,7 +21,7 @@ class TestActivityCallOperationNodeSuite < Test::Unit::TestCase
   end
  
   def test_result_assigned_to_class
-    assertRegexInFile('acceleo/testing/call_operation/autogen/Activity1.rb', '^\s*saveThis \= Class1.operation1.*')
+    assertRegexInFile('acceleo/testing/call_operation/autogen/Activity1.rb', '^\s*saveThis\s*\=\s*Class1.operation1.*')
   end
   
   def test_call_params_sorted_alphabetically
@@ -37,5 +38,19 @@ class TestActivityCallOperationNodeSuite < Test::Unit::TestCase
   
   def test_class_operation_with_no_parameter
     assertRegexInFile('acceleo/testing/call_operation/autogen/Activity1.rb', '^\s*Class2.operation1\(\)')
+  end
+  
+  def test_multi_dependency
+    autogen = "acceleo/testing/call_operation/autogen/Activity1.rb"
+
+    require_statements = Set.new []
+    File.open(autogen).each do |line|
+      if (line =~ /^require.+\'.+\'.*/)
+        puts line
+        uniqueAdd = require_statements.add?(line);
+
+        assert_not_nil(uniqueAdd, "Duplicate require statement found: " + line)
+      end
+    end
   end
 end
