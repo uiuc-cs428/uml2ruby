21d6d3e8086d0f61ff11c5ce4e9e3c2ed97a3ddb
David Overgaard <enoch137@gmail.com>
2014-04-27 01:29:23 -0600

    Update a failing unit test based on white space error to use regex instead.

--- acceleo/testing/activity_parameter_node/ruby_unit/activity_parameter_node_test.rb
+++ acceleo/testing/activity_parameter_node/ruby_unit/activity_parameter_node_test.rb
@@ -3,22 +3,33 @@ require 'rubygems'
 require 'tuxml'
 
 class TestActivityNodeSuite < Test::Unit::TestCase
-  def stringInFile(fileName, string)
-    generatedCode = File.open(fileName).read
-    assert(generatedCode.include?(string))
+  def assertRegexInFile(fileName, string)
+    assert(regexFoundInFile(fileName, string), "String not found: " + string)      
   end
-    
+
+  def regexFoundInFile(fileName, string)
+    File.open(fileName, "r") do |file|
+      file.each do |line|
+        if(  line.match(string) )
+          return true
+        end
+      end
+  
+      return false
+    end
+  end
+      
   def test_activity_behavior_defined
-    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'def Activity1(input1,')
+    assertRegexInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'def Activity1\(.*input1')
   end
   
   def test_activity_parameter_node_initialization
-    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput1 = input1')
-    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput2 = input2')
-    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput3 = nil')
+    assertRegexInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput1 = input1')
+    assertRegexInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput2 = input2')
+    assertRegexInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'anInput3 = nil')
   end
   
   def test_activity_return_statement
-    stringInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'return anInput3')
+    assertRegexInFile('acceleo/testing/activity_parameter_node/autogen/Activity1.rb', 'return anInput3')
   end
 end
